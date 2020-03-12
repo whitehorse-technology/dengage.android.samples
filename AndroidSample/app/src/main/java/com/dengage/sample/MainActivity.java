087560b345ccd0c6095bfc5bc749d9c6b834963c
Ramazan Dönmez <ramazan@whitehorse.technology>
2020-03-12 14:38:13 +0300

    init

--- AndroidSample/app/src/main/java/com/dengage/sample/MainActivity.java
+++ AndroidSample/app/src/main/java/com/dengage/sample/MainActivity.java
@@ -4,6 +4,7 @@ import android.app.AlertDialog;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 
 import com.dengage.sdk.DengageManager;
