820341c244fa10ffb58f23efb76f55aeb2856218
Ramazan Dönmez <ramazan.dnmz@gmaiil.com>
2020-05-27 16:59:14 +0300

    upgrade sdk.

--- dengage.android.push.firebase.sample/app/src/main/java/dengage/android/push/firebase/sample/Constants.java
+++ dengage.android.push.firebase.sample/app/src/main/java/dengage/android/push/firebase/sample/Constants.java
@@ -6,11 +6,11 @@ public class Constants {
     final static String INTEGRATION_KEY = "CfTPM12P4IpYUHHDSN36Bi3PKIx7CZNkXxw_p_l_MfAbLQaXT10_p_l_A6EWBj6_p_l_aM2G2kO_s_l_TEIJ4PLJkPhhLgjb8qlUHhnhEIb7HgxGa4_p_l_7UM6atjlxk9EfhFCMV_p_l_6NOfmBhvBZ";
 
     // TEST
-    //final static String INTEGRATION_KEY = "CfTPM12P4IpYUHHDSN36Bi3PKIx7CZNkXxw_p_l_MfAbLQaXT10_p_l_A6EWBj6_p_l_aM2G2kO_s_l_TEIJ4PLJkPhhLgjb8qlUHhnhEIb7HgxGa4_p_l_7UM6atjlxk9EfhFCMV_p_l_6NOfmBhvBZ";
+    //final static String INTEGRATION_KEY = "ui4U5Zs8jTl7xg73Yb9WOJX9cwgzte_p_l_n6nnn_s_l_UcEpS5AJH8W3H_s_l_vQqxb8okaR929ebrEhJoomqCTnWD7m_s_l_sPM_p_l_sQQE_p_l_VIymyKOBpHapj6ff6anfHvqozlh8077DHTPRF";
 
     // PREPROD
-    //final static String INTEGRATION_KEY = "CfTPM12P4IpYUHHDSN36Bi3PKIx7CZNkXxw_p_l_MfAbLQaXT10_p_l_A6EWBj6_p_l_aM2G2kO_s_l_TEIJ4PLJkPhhLgjb8qlUHhnhEIb7HgxGa4_p_l_7UM6atjlxk9EfhFCMV_p_l_6NOfmBhvBZ";
+    //final static String INTEGRATION_KEY = "EUEuU6s83qUSL5Kos6wk9gxb95tS1ewj_s_l_ZUApQJVaApCjAfhZwixYgKBb_p_l_h3XJ_p_l_WDOX02PkhfLPpnun3g1ONaaz3dAGLcWbha9ksj6b6s26GLaM3_p_l_n2u3K4NNyNfVQ5x";
 
     // PROD ULUS
-    //final static String INTEGRATION_KEY = "CfTPM12P4IpYUHHDSN36Bi3PKIx7CZNkXxw_p_l_MfAbLQaXT10_p_l_A6EWBj6_p_l_aM2G2kO_s_l_TEIJ4PLJkPhhLgjb8qlUHhnhEIb7HgxGa4_p_l_7UM6atjlxk9EfhFCMV_p_l_6NOfmBhvBZ";
+    //final static String INTEGRATION_KEY = "rRfDyVrkSu_s_l_Sr3fzjo_p_l_lxfyB86Jj5J6e_s_l_eye3uOuRmhF4eDx59LSutQaKefvlDh5hhvtw87MM_s_l_l_p_l_jHkBeXqQ0bBEEfQxSy6CT9uFUJCcJfduovNq5nicOSOlFi3iUNCT";
 }
