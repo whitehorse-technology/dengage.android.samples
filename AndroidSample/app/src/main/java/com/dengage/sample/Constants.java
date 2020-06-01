b78184d506464691334b64ee3f762af8ffd44e2b
Ramazan Dönmez <ramazan.dnmz@gmaiil.com>
2020-06-01 20:18:59 +0300

    upgrade sdk.

---
+++ AndroidSample/app/src/main/java/com/dengage/sample/Constants.java
@@ -0,0 +1,18 @@
+package com.dengage.sample;
+
+public class Constants {
+    // PROD CHADAS
+    //final String INTEGRATION_KEY = "x9V_p_l_TEhVz_s_l_2Bho_s_l_lsfjMIsjEjGECyDzoldLHF7G3gi1GiUJBSJujlUqmeWN9dcOiTgpVSqlCybo83sNQVeTUbMo559FIeyL5XXOj_p_l_p4XidRgb2KA_s_l_3t_p_l_jcMpo_s_l_2aKrgG";
+
+    // PROD ULUS
+    final static String INTEGRATION_KEY = "BL6fzkB6OwHEoXvvsB8pPiYg98jETXv_s_l_RaEUIdzcI4StbCnX5J77bHhpL0PIbTdzVwsXlp9MGBvksJ3QnZlmNIujfBOreAQkKDf_s_l_RsYogZQqWP60wO7tam4vGCpgSxCE";
+
+    // DEV
+    //final static String INTEGRATION_KEY = "GXIAyymEST9TQWVfhajeX_p_l_4wCma8kEvgV6BSPj1ZQAKFYiCd8RsZWocCMiP_p_l_5K5XEwiYiMihOTY9ZBT_s_l_LnNzx9f_s_l_cBF2hN3oIoPVu8gHtZ0nP7ANCFljvvYL489SvdgA";
+
+    // TEST
+    // final static String INTEGRATION_KEY = "Evke8NhhgLX6Gwo5QeggzCMFVloTnGjUSf0oWPUQM7mQxsia4pRkSuqCqhPtu_s_l_q7Ye9PemJYerIjEmd8qbEVNQL2_s_l__s_l_gN6vmS25Aj2EMk_s_l_2k1us1JkrjFgT3kbEDjuZqq";
+
+    // PREPROD
+    //final static String INTEGRATION_KEY = "ovoklhOER8JpdF5fB7vqe86T_p_l_iNDdU9C4veOIb0hCvbTB3RW7ZijhqERdbY7_s_l_mY6OIimKlyA2FqBfH3El3KCZ0Z_s_l_zcex_s_l_0pGfKEcs10MRbKbLdCdnF4r5n_p_l_8b8QAmlJI";
+}
---
+++ AndroidSample/app/src/main/java/com/dengage/sample/DeepLinkActivity.java
@@ -0,0 +1,61 @@
+package com.dengage.sample;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+public class DeepLinkActivity extends AppCompatActivity {
+
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.content_deeplink);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        Context context = getApplicationContext();
+
+        if (getIntent().getAction().equals(Intent.ACTION_VIEW)) {
+            Uri data = getIntent().getData();
+
+            if (data != null) {
+
+                Log.d("DenPush", data.toString());
+                Log.d("DenPush", getIntent().getAction().toString());
+            }
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
