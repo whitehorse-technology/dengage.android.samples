c0bd39e0402a87548f086d6ca5b9e932bda77754
Ramazan Dönmez <ramazan.dnmz@gmaiil.com>
2020-06-08 12:40:51 +0300

    upgrade sdk

--- dengage.android.push.huawei.sample/app/src/main/java/dengage/android/push/huawei/sample/Constants.java
+++ dengage.android.push.huawei.sample/app/src/main/java/dengage/android/push/huawei/sample/Constants.java
@@ -9,8 +9,8 @@ public class Constants {
     //final static String INTEGRATION_KEY = "8hVd3R5Fzmt_s_l_FJv0GSxtWig1mOb0NXiWYXRLE3u_p_l_F29ECOEPNoTl9U6_p_l_UvkxfFbMNG3vvAYZo4CobIQ5njHhIDTdhTrfiAHYG1O3DkGXEf_s_l_r5Ow1TvboFCSZpDoU_p_l_1Fo";
 
     // PREPROD
-    final static String INTEGRATION_KEY = "NAunkrm9uIcJ30q_p_l_Lu58b48MOXY9sqR0pvfzBb2pnJmMCdbztKypPyKcsM1IeD3rFyGMj_p_l_1_p_l_PDvZFES3cgX6qkOXLdlY1WNcJbmnCaAVqDpRiWh2YuH2He3SwPCfCJRs";
+    //final static String INTEGRATION_KEY = "NAunkrm9uIcJ30q_p_l_Lu58b48MOXY9sqR0pvfzBb2pnJmMCdbztKypPyKcsM1IeD3rFyGMj_p_l_1_p_l_PDvZFES3cgX6qkOXLdlY1WNcJbmnCaAVqDpRiWh2YuH2He3SwPCfCJRs";
 
     // PROD ULUS
-    //final static String INTEGRATION_KEY = "utGSmJr67Y7sCYiiMFz1UCNATrwk3fy7N4GMCw_p_l_Kk9_s_l_f0xRcI2gi03u1HL6kUwHKiWV_s_l__s_l_iXrdsAKafYbhcsb6F3Qg1f8kPqF_s_l_tu2Z_s_l_HEBcR2czCv7KWr7O2J_p_l_B1VBtiJ";
+    final static String INTEGRATION_KEY = "utGSmJr67Y7sCYiiMFz1UCNATrwk3fy7N4GMCw_p_l_Kk9_s_l_f0xRcI2gi03u1HL6kUwHKiWV_s_l__s_l_iXrdsAKafYbhcsb6F3Qg1f8kPqF_s_l_tu2Z_s_l_HEBcR2czCv7KWr7O2J_p_l_B1VBtiJ";
 }
