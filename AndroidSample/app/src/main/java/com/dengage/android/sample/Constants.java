b5d64965ba12cc8667d3ce0620b0fc3e4df9199c
Ramazan Dönmez <ramazan.dnmz@gmaiil.com>
2020-06-30 15:06:47 +0300

    upgrade sdk & implement huawei

---
+++ AndroidSample/app/src/main/java/com/dengage/android/sample/Constants.java
@@ -0,0 +1,9 @@
+package com.dengage.android.sample;
+
+public class Constants {
+ 
+    public static String FIREBASE_INTEGRATION_KEY = "YOUR_INTEGRATION_KEY";
+
+    public static String HUAWEI_INTEGRATION_KEY = "YOUR_INTEGRATION_KEY";
+
+}
---
+++ AndroidSampleEcomm/app/src/main/java/com/iqonic/shophop/Constants.java
@@ -0,0 +1,20 @@
+package com.iqonic.shophop;
+
+public class Constants {
+
+    // DEV
+    //public final static String INTEGRATION_KEY = "gMpj3Gwg4PqRh2CWb_s_l_U3qfoz3_p_l_DoVcMJvls47i5B8sg4GG4f9Qe0QcSQBp2lmkbN0pS0nXTpCQnZmHmUnN8HpkgsGFeDOjcUYdvWzSrhzx4CYatlXEvHLJVemp6FxsoX";
+
+    // PROD ECOMM
+    public final static String INTEGRATION_KEY = "0Hd1cVPPHulDXt_p_l_7hTCzPhXJ3LIS2WS9CyAEpqrDQ4SgAL3EjUmcztfO5aVuuvEo3u_s_l_wNiCgWgiAMg1JkonHghrkGyj9DigRNBGuyjnBznnLY_p_l_vP4EHKgMdNcJpK2jFT";
+
+    // TEST
+    //public final static String INTEGRATION_KEY = "jQ4EAJITLkF5Hr_s_l_DK_p_l_Fb6mytXnsi7PZ4qYmYwJbl_s_l_r9v4M9WY4gSJy3obSVo4uOc3dnNwrrADilIkWrlUtWcUbRW_p_l_XFdMX7fsq5PJ6l90suiXuAMXtKrDW9jpMrbzuJp";
+
+    // PREPROD
+    //public final static String INTEGRATION_KEY = "Z22a7YCzVCFtDkyo68uCYegBb3VgbRpKmiMeTLS57RV6grZHUmM4ik4c9s_p_l_FMwh_p_l_JDDTgkP9fWFd7avZDQF9FzhIyKKNBj6NHDbnOybiZTVQ36KLT0Alb1qdjOFIpevi";
+
+    // PROD ULUS
+    //public final static String INTEGRATION_KEY = "l1uNZU67v4_p_l_7YT_s_l_82j8u0OXRqbu9hvM9oZpsqy0zfoHftYQSLn1Tf0oCefs3r3JsAiWs8KAqrlqwilb37R7CL7lva8TIrnRy3gHEYHqePsqIqTLohQhrzUV5ab8LWkg_s_l_";
+
+}
